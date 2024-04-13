effc99ddf69cd6c1daf63b31f4b7293369498370
anisberto reis <anisberto.reis@e-unicred.com.br>
2024-04-13 12:56:31 -0300

    feat: create the index file and basic script to presentation project

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Starter</title>
+</head>
+<body>
+<h1>Welcome to the Starter PHP</h1>
+<?php echo 'ok! This section is PHP code...'; ?>
+</body>
+</html>
